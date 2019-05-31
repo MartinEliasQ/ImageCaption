9fdbc9a587609df0d840b6cd2c6c9ae37168d577
martineliasq <elias.quintero@udea.edu.co>
2019-05-31 16:40:38 -0500

    add model

--- icg/processing/image.py
+++ icg/processing/image.py
@@ -10,27 +10,40 @@ from keras.models import Model
 from keras import backend as K
 
 
-def extract_feactures(directory):
+def extract_features(directory):
+    # load the model
     model = VGG19(weights='imagenet')
+    # re-structure the model
     model.layers.pop()
     model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    # summarize
+    print(model.summary())
+    # extract features from each photo
     features = dict()
     for name in listdir(directory):
-        filename = "{}/{}".format(directory, name)
+        # load an image from file
+        filename = directory + '/' + name
         image = load_img(filename, target_size=(224, 224))
+        # convert the image pixels to a numpy array
+        image = img_to_array(image)
+        # reshape data for the model
         image = image.reshape(
             (1, image.shape[0], image.shape[1], image.shape[2]))
+        # prepare the image for the VGG model
         image = preprocess_input(image)
+        # get features
         feature = model.predict(image, verbose=0)
+        # get image id
         image_id = name.split('.')[0]
+        # store feature
         features[image_id] = feature
-        print('{}'.format(name))
+        print('>%s' % name)
     return features
 
 
 def save_feactures(feactures, dest):
     try:
-        dump(features, open('features.pkl', 'wb'))
+        dump(features, open(dest, 'wb'))
     except:
         print("Some error saving pkl file")
 
